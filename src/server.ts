24ae87b5caafea227fce10650fd4f424fbb7a919
rhuanmaciel <rhuanc01@gmail.com>
2025-12-11 18:09:18 -0300

    fix(cors): add PUT method to allowed CORS methods for user preferences update

--- src/server.ts
+++ src/server.ts
@@ -16,7 +16,7 @@ app.use(express.json());
 // CORS
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   if (req.method === 'OPTIONS') {
     return res.sendStatus(204);
